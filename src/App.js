9d5d6abc9dd3dc2d58e5b5265850e178ce927d82
Stefanie <stefanie@workwithhuddle.com>
2022-08-01 13:13:04 +0100

    chore: initial commit. feat: fetchng data from api and outputting in list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect } from 'react';
+import axios from 'axios';
+import EventList from './components/EventList/EventList';
 
 function App() {
+  const [events, setEvents] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://t-bridge.s3.eu-west-1.amazonaws.com/events.json')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(currentPageUrl).then(res => {
+      setLoading(false)
+      setEvents(res.data.data.getEvents.map(events => events))
+    })
+  }, [currentPageUrl])
+
+  if (loading) return (
+    <p className="eventListing__loading">Loading...</p>
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="eventListing">
+      <div className="eventListing__container">
+        <EventList  events={events}/>
+      </div>
     </div>
   );
 }
